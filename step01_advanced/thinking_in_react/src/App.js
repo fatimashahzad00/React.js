90c8ce0481cf0b16ea02b3ec11ba1471ca475a85
Fatima Shahzad <fatimashahzad062@gmail.com>
2023-10-11 21:25:35 +0500

    thinking_in_react

---
+++ step01_advanced/thinking_in_react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Appprops from "./useState/Index";
+import AppState from "./useState/Index";
+
+export default function App() {
+  return(
+    <>
+    <Appprops />
+    <AppState />
+    </>
+    
+  )
+}
