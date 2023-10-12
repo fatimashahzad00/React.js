507cead68b5dc6cf8de8ac7e3279f4d0360a7b5e
Fatima Shahzad <fatimashahzad062@gmail.com>
2023-10-12 10:08:22 +0500

    Javascript JSX with Curly Braces

---
+++ step01_advanced/04_jsx_curlybraces/src/App.js
@@ -0,0 +1,29 @@
+
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 's',
+  getImageUrl: 'https://rb.gy/0ntca',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={person.getImageUrl}
+        alt={person.name}
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
