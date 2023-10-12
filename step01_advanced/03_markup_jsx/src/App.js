de593f845c69f6016e0532e97c09db5db8c3ee02
Fatima Shahzad <fatimashahzad062@gmail.com>
2023-10-12 09:11:47 +0500

    writing markup with jsx

---
+++ step01_advanced/03_markup_jsx/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='bio'>
+      <div className="intro">
+        <h1 >Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br /><br />
+        <b>And <i>pictures</i></b> of scientists!
+      </p>
+    </div>
+  );
+}
